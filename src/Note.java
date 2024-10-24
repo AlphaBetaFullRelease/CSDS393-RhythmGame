a5a8263947b62350c23e2df5354da4501c17b123
SlimSchliesy <crs270@case.edu>
2024-10-24 10:45:26 -0400

    Update Note.java

--- src/Note.java
+++ src/Note.java
@@ -3,5 +3,10 @@ public class Note {
     // position (goes from 0-1 where 0 is beginning of the screen and 1 is the end of the screen)
     public double pos = 0;
 
-    public Note(){}
+    // which of the 4 possible columns the note will appear in(where 0-3 is left-more to right-most column)
+    public int col;
+    
+    public Note(int col){
+        this.col = col;
+    }
 }
