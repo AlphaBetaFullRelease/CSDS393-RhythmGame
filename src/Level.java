2a3a9f3f0cdb0e6e7cfc6501e4f0d93382406203
AlphaBetaFullRelease <43766687+AlphaBetaFullRelease@users.noreply.github.com>
2024-11-11 23:47:33 -0500

    Initial level parser and loader built Still needs tested before being merged with main

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //holds all static data about a level & provides functions to load / save a level file
 public class Level {
     //store note data (kept in order by spawn time)
@@ -56,8 +61,96 @@ public class Level {
     }
 
     //loads a level from a json file
-    public Level loadFromFile(String path) {
-        return null;
+    public static Level loadFromFile(String path) throws FileNotFoundException {
+        // do you think dr chaudhary would kill me if he saw this jerry-rigged json parser??
+
+        // i think under this current implementation a json file with { [ } or ] inside a
+        // string value will cause this to throw a parse error. however i don't see any
+        // reason for that scenario to arise in normal use unless the end user specifically
+        // messes with the level files after creation
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        String openBrackets = "";
+        String title = "", author = "";
+        boolean colonSeen = false, inString = false;
+        String key = "", value = "";
+        int lane = -1, tempo = -1;
+        double time = -1, duration = -1, delay = -1;
+        long pos = -1;
+        ArrayList<ArrayList<StoredNote>> notes = new ArrayList<>(4);
+        for (int i = 0; i < 4; i++) {
+            notes.add(new ArrayList<>());
+        }
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            colonSeen = false;
+            inString = false;
+            key = "";
+            value = "";
+            for (char c : line.toCharArray()) {
+                if (c == '{' || c == '[') {
+                    openBrackets += c;
+                } else if (c == '"') {
+                    if (openBrackets.lastIndexOf('"') == openBrackets.length() - 1) {
+                        openBrackets = openBrackets.substring(0, openBrackets.length() - 1);
+                        inString = false;
+                    } else {
+                        openBrackets += '"';
+                        inString = true;
+                    }
+                } else if (c == '}') {
+                    if (openBrackets.lastIndexOf('{') == openBrackets.length() - 1) {
+                        openBrackets = openBrackets.substring(0, openBrackets.length() - 1);
+                    } else {
+                        throw new RuntimeException("Parse error: mismatched brackets");
+                    }
+                } else if (c == ']') {
+                    if (openBrackets.lastIndexOf('[') == openBrackets.length() - 1) {
+                        openBrackets = openBrackets.substring(0, openBrackets.length() - 1);
+                    } else {
+                        throw new RuntimeException("Parse error: mismatched brackets");
+                    }
+                } else if (c == ':') {
+                    colonSeen = true;
+                } else if (inString) {
+                    if (!colonSeen) {
+                        key += c;
+                    } else {
+                        value += c;
+                    }
+                }
+            }
+
+            System.out.printf("%s: %s\n", key, value);
+
+            if (key.equals("name")) {
+                title = value;
+            } else if (key.equals("author")) {
+                author = value;
+            } else if (key.equals("delay")) {
+                delay = Integer.parseInt(value);
+            } else if (key.equals("tempo")) {
+                tempo = Integer.parseInt(value);
+            } else if (key.equals("time")) {
+                time = Double.parseDouble(value);
+                pos = (int) ((60 / tempo * time + delay) * 1000);
+            } else if (key.equals("lane")) {
+                lane = Integer.parseInt(value);
+            } else if (key.equals("duration")) {
+                duration = Double.parseDouble(value);
+                notes.get(lane).add(new StoredNote(pos, lane));
+            }
+        }
+
+        StoredNote[][] tracks = {new StoredNote[notes.get(0).size()], new StoredNote[notes.get(1).size()], new StoredNote[notes.get(2).size()], new StoredNote[notes.get(3).size()]};
+        for (int i = 0; i < tracks.length; i++) {
+            for (int j = 0; j < tracks[i].length; j++) {
+                tracks[i][j] = notes.get(i).get(j);
+            }
+        }
+
+        return new Level(title, author, tracks);
     }
     
     //get the size of noteGrid[i]
@@ -89,5 +182,13 @@ public class Level {
 
         // no note can be spawned
         return null;
+    }
+
+    public static void main(String[] args) {
+        try {
+            loadFromFile("src\\test.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
