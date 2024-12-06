c21c356160a293dfa6e9280314465d7fbdc25fde
AlphaBetaFullRelease <43766687+AlphaBetaFullRelease@users.noreply.github.com>
2024-12-06 17:04:07 -0500

    files copied over to fix errors

--- src/LevelSelect.java
+++ src/LevelSelect.java
@@ -1,65 +1,158 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
-
 public class LevelSelect extends JPanel implements ActionListener, Scene {
-    // reference to the sceneRunner so scenes can be changed
-    private SceneRunner sceneChanger;
-
-    // reference to all the levels
-    private ArrayList<Level> levels = new ArrayList<>();
-    private ArrayList<LevelCard> displayList = new ArrayList<LevelCard>();
+    // level card list
+    private ArrayList<LevelCard> cardList = new ArrayList<>();
+    // number of cards shown per page
+    private final int pageMax = 5;
+    // number of pages in use, minimum is always 1
+    private int numPages = 1;
+    // current page number
+    private int page;
+    //graphics handler for level select
     private final LevelSelectGraphics graphicsHandler;
-    public LevelSelect(Level level) {
-    	// JPanel properties
-        this.setPreferredSize(new Dimension(800, 450));
-        this.setFocusable(true);
-        //read level data and store in levels array
-        displayList.add(new LevelCard(level));
-        //create displayList
-        
-        //initialize graphics handler
-        graphicsHandler = new LevelSelectGraphics(displayList);
+    // reference to the sceneRunner (so that scenes can be changed)
+    private SceneRunner sceneChanger;
+    // user data object for reading level & score files
+    private UserData userData;
+    // constructor, create level cards from level data and draw the graphics
+    public LevelSelect() {
+        // initialize user data object
+        userData = new UserData();
+        // load level data and populate card list
+        loadLevelsCards();
+        // set current page as first page
+        page = 1;
+        // initialize the GUI, pass this object as parameter
+        graphicsHandler = new LevelSelectGraphics(this);
     }
-    
-    @Override
-    public void paint (Graphics graphics) {
-        // cast graphics to 2D
-        Graphics2D g = (Graphics2D) graphics;
 
-        // clear the previous frame
-        super.paintComponent(g);
-
-        // tell GameGraphics to draw the frame
-        graphicsHandler.drawFrame(g);
-    }
-    
-    // sets reference for the sceneChanger
-    @Override
-    public void setSceneRunner(SceneRunner sceneRunner){
-        sceneChanger = sceneRunner;
-    }
-    
     @Override
     public void update(long delta) {
-    	repaint();
+        // does nothing since we are not using frames for this UI
     }
-    
+
+    @Override
+    public void setSceneRunner(SceneRunner sceneRunner) { sceneChanger = sceneRunner; }
+
+    // cast this object as JPanel and return it
     @Override
     public JPanel getPanel() {
-    	return (JPanel)this;
+        return (JPanel)this;
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        //
+        //No implementation...
     }
-    
-    //method to load local level data into the displayList array
-    private void populateDisplayList() {
-    	//
+    // get level cards in current page
+    public ArrayList<LevelCard> getPageCards() {
+        //create arraylist of size pageMax
+        ArrayList<LevelCard> pageCards = new ArrayList<>(pageMax);
+        //iterate through cardList
+        int pos = pageMax * (page - 1);
+        for (int i = pos; i < cardList.size() && i < pos + pageMax; i ++)
+            pageCards.add(cardList.get(i));
+        return pageCards;
+    }
+    // change cardList pages (true = next page, false = prev page)
+    public void changePage(boolean nextP) {
+        // page logic
+        if (nextP) {
+            page += 1;
+            if (page > numPages) page = 1;
+        } else {
+            page -= 1;
+            if (page < 1) page = numPages;
+        }
+        // refresh graphics
+        graphicsHandler.refreshList();
+    }
+    // get current page number
+    public int getPage() { return page; }
+    // get number of pages
+    public int getNumPages() { return numPages; }
+    // get max number of cards per page
+    public int getPageMax() { return pageMax; }
+    // get user data
+    public UserData getUserData() { return userData; }
+    // method to sort levelCard list by Title A-Z
+    public void sortCardsTitle() {
+        for (int i = 0; i < cardList.size() - 1; i ++) {
+            for (int j = i + 1; j < cardList.size(); j ++) {
+                String str1 = cardList.get(i).getTitle();
+                String str2 = cardList.get(j).getTitle();
+                if (str1.compareTo(str2) > 0) {
+                    LevelCard temp = cardList.get(i);
+                    cardList.set(i, cardList.get(j));
+                    cardList.set(j, temp);
+                }
+            }
+        }
+    }
+    // method to sort levelCard list by Author A-Z
+    public void sortCardsAuthor() {
+        for (int i = 0; i < cardList.size() - 1; i ++) {
+            for (int j = i + 1; j < cardList.size(); j ++) {
+                String str1 = cardList.get(i).getCreator();
+                String str2 = cardList.get(j).getCreator();
+                if (str1.compareTo(str2) > 0) {
+                    LevelCard temp = cardList.get(i);
+                    cardList.set(i, cardList.get(j));
+                    cardList.set(j, temp);
+                }
+            }
+        }
+    }
+    // method to sort levelCard list by Difficulty, asc determines sort order
+    public void sortCardsDiff(boolean asc) {
+        for (int i = 0; i < cardList.size() - 1; i ++) {
+            for (int j = i + 1; j < cardList.size(); j ++) {
+                Integer int1 = Integer.valueOf(cardList.get(i).getDifficulty());
+                Integer int2 = Integer.valueOf(cardList.get(j).getDifficulty());
+                if (asc ? (int1.compareTo(int2) > 0) : (int1.compareTo(int2) < 0)) {
+                    LevelCard temp = cardList.get(i);
+                    cardList.set(i, cardList.get(j));
+                    cardList.set(j, temp);
+                }
+            }
+        }
+    }
+    // load levels from level folder
+    public void loadLevelsCards() {
+        // have userData load the level and score data
+        userData.loadLevelData();
+        userData.loadScoreData();
+        // clear card list
+        cardList.clear();
+        // create cardList
+        for (Level level : userData.getLevels())
+            cardList.add(new LevelCard(level, userData.getLevelScore(level)));
+        // calculate the number of pages
+        numPages = (int) Math.ceil((double) (cardList.size() + 1) / pageMax);
+        // sort cards by Title A-Z
+        sortCardsTitle();
+    }
+    // change scene to main menu
+    public void exitToMenu() {
+        System.out.println("return to main menu");
+        Settings settings = null;
+        try {
+            settings = new Settings();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        sceneChanger.changeScene(settings);
+        //sceneChanger.changeScene(new LevelInfo());
+    }
+    // change scene to game
+    public void playLevel(Level l) {
+        // pass level to new game scene and change scenes
+        sceneChanger.changeScene(new Game(l));
+        //sceneChanger.changeScene(new LevelInfo(l));
     }
 }
--- src/LevelSelectGraphics.java
+++ src/LevelSelectGraphics.java
@@ -1,114 +1,308 @@
-import java.awt.BasicStroke;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.awt.Color;
-
-//does all of the graphical work for the level select
-//implementation copied from GameGraphics for the purposes of the demo
+// does all of the graphical work for the level select
 public class LevelSelectGraphics {
-	
-	private ArrayList<LevelCard> list;
-	
-    //these variables control the layout of the level select (all in % of the screen)
-    private static final double headerHeight = 0.1; // the height of the level select header
-    private static final double listWidth = 0.75; // the width of the level list
-    private static final double cardHeight = 0.2; // height of a single card
-    private static final double[] cardTextHeight = {cardHeight * 0.33, cardHeight * 0.66}; //height of card (top and bottom) text
-    private static final double cardTextMargin = 0.05; //margin between card border and text
+    // reference to level select object
+    private LevelSelect levelSelect;
+    // level select JPanel
+    private JPanel mainPanel;
+    // page number display
+    private JLabel pageDisplay;
+    // list display
+    private ListDisplay listDisplay;
+    // layout info
+    private final int width = 800;
+    private final int height = 450;
+    private final int cardWidth = 600;
+    private final int cardHeight = 80;
+    private final int navButtonY = 130;
+    private final int navButtonWidth = 50;
+    private final int navButtonHeight = 100;
+    private final int contButtonWidth = 100;
+    private final int contButtonHeight = 50;
+    private final int pageNumberWidth = 50;
+    // colors
+    private final Color cHeader = new Color(151, 109, 194);
+    private final Color cButton = new Color(106,65,148);
+    private final Color cBody = new Color(138, 121, 156);
+    private final Color cCard = new Color(71,49,94);
+    private final Color cDiffStar = Color.red;
+    private final Color cDiffCont = Color.black;
 
-    //needs to be passed the displayList
-    public LevelSelectGraphics(ArrayList<LevelCard> l){
-        this.list = l;
-
-        //generate all dimensions for graphical objects
-        Layout.initialize();
-    }
-
-    //draws all the elements to the screen
-    public void drawFrame(Graphics2D g){
-        //set default line thickness
-        g.setStroke(new BasicStroke(2f));
-        //draw the level select header
-        drawHeader(g);
-        //draw the level list
-        drawList(g);
-    }
-    
-    //
-    private void drawHeader(Graphics2D g) {
-    	//fill in header background
-    	g.setColor(Colors.headerFill);
-    	g.fillRect(0, 0, getWidth(), Layout.headerH);
-    	
-    	//draw header text
-    	g.setColor(Colors.border);
-    	g.drawString("Level Select", 10, (int)Layout.headerH/2);
-    	
-    	//draw header border
-    	g.drawLine(0, Layout.headerH, getWidth(), Layout.headerH);
-    }
-    
-    private void drawCard(Graphics2D g, LevelCard card, int pos) {
-    	//calculate y position
-    	int cardPos = Layout.headerH + (Layout.cardH * pos);
-    	
-    	//fill in card background
-    	g.setColor(Colors.cardFill);
-    	g.fillRect(Layout.listLeft, cardPos, Layout.listW, Layout.cardH);
-    	
-    	//draw card text
-    	g.setColor(Colors.border);
-    	g.drawString(card.getHeader(), Layout.listLeft + Layout.textM, cardPos + Layout.textH[0]); //card header
-    	g.drawString(card.getDuration(), Layout.listLeft + Layout.textM, cardPos + Layout.textH[1]); //duration
-    	g.drawString(card.getDifficulty(), getWidth() - Layout.listLeft - Layout.textM, cardPos + Layout.textH[0]); //difficulty
-    	g.drawString("000000", getWidth() - Layout.listLeft - Layout.textM, cardPos + Layout.textH[1]); //score
-    	
-    	//draw card border
-    	g.drawRect(Layout.listLeft, (Layout.cardH * pos) + Layout.headerH, Layout.listW, Layout.cardH);
-    	
-    }
-    //
-    private void drawList(Graphics2D g) {
-    	for(int i = 0; i < list.size(); i ++) {
-    		drawCard(g, list.get(i), i);
-    	}
-    }
-
-    public static int getWidth(){
-        return 800;
-    }
-
-    public static int getHeight(){
-        return 450;
-    }
-
-    //holds all coordinates that only need to be calculated once
-    private static class Layout {
-    	public static int headerH;
-    	public static int listLeft; //margin between left side of the screen and the level list
-    	public static int cardH;
-    	public static int listW;
-    	public static int[] textH = new int[2];
-    	public static int textM;
-        //uses the variables in GameGraphics to generate coordinates for graphical elements
-        public static void initialize(){
-        	headerH = (int)(headerHeight * getHeight());
-        	listLeft = (int)(((1 - listWidth)/2) * getWidth());
-        	cardH = (int)(cardHeight * getHeight());
-        	listW = (int)(listWidth * getWidth());
-        	textH[0] = (int)(cardTextHeight[0] * getHeight());
-        	textH[1] = (int)(cardTextHeight[1] * getHeight());
-        	textM = (int)(cardTextMargin * getWidth());
+    public LevelSelectGraphics(LevelSelect s) {
+        // get level select object
+        this.levelSelect = s;
+        // get level select as a JPanel
+        this.mainPanel = s.getPanel();
+        // JPanel properties
+        mainPanel.setPreferredSize(new Dimension(width, height));
+        mainPanel.setFocusable(true);
+        mainPanel.setLayout(null);
+        // initialize panels to which we will add each UI component
+        // header
+        JPanel pHeader = new JPanel();
+        pHeader.setLayout(null);
+        pHeader.setBounds(0, 0, width, contButtonHeight);
+        pHeader.setBackground(cHeader);
+        // body
+        JPanel pBody = new JPanel();
+        pBody.setLayout(null);
+        pBody.setBounds(0, contButtonHeight, width, height - contButtonHeight);
+        pBody.setBackground(cBody);
+        // create interactive elements
+        // sort types dropdown
+        JComboBox bSort = new JComboBox(new String[]{"Title A-Z","Author A-Z","Diff Asc", "Diff Desc"});
+        bSort.setForeground(Color.white);
+        // action listener to tell level select to sort cards according to selection
+        bSort.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        String s = (String) bSort.getSelectedItem();
+                        switch (s) {
+                            case "Title A-Z":
+                                levelSelect.sortCardsTitle();
+                                break;
+                            case "Author A-Z":
+                                levelSelect.sortCardsAuthor();
+                                break;
+                            case "Diff Asc":
+                                levelSelect.sortCardsDiff(true);
+                                break;
+                            case "Diff Desc":
+                                levelSelect.sortCardsDiff(false);
+                                break;
+                        }
+                        // refresh graphics
+                        refreshList();
+                    }
+                }
+        );
+        bSort.setBackground(cButton);
+        TitledBorder sortBorder = BorderFactory.createTitledBorder("Sort");
+        sortBorder.setTitleColor(Color.white);
+        bSort.setBorder(sortBorder);
+        bSort.setBounds(width - contButtonWidth, 0, contButtonWidth, contButtonHeight);
+        pHeader.add(bSort);
+        // navigation buttons, only drawn if there is more than one page of cards
+        if (levelSelect.getNumPages() > 1) {
+            // previous page button
+            JButton bPrev = new JButton("<");
+            bPrev.setForeground(Color.white);
+            // add action listener to go to previous page
+            bPrev.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            levelSelect.changePage(false);
+                        }
+                    }
+            );
+            bPrev.setBackground(cButton);
+            bPrev.setBounds(0, navButtonY, navButtonWidth, navButtonHeight);
+            pBody.add(bPrev);
+            // next page button
+            JButton bNext = new JButton(">");
+            bNext.setForeground(Color.white);
+            // add action listener to go to next page
+            bNext.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            levelSelect.changePage(true);
+                        }
+                    }
+            );
+            bNext.setBackground(cButton);
+            bNext.setBounds(width - navButtonWidth, navButtonY, navButtonWidth, navButtonHeight);
+            pBody.add(bNext);
         }
+        // main menu button
+        JButton bExit = new JButton("settings");
+        bExit.setForeground(Color.white);
+        // add event listener to tell level select to switch scenes
+        bExit.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        levelSelect.exitToMenu();
+                    }
+                }
+        );
+        bExit.setBackground(cButton);
+        bExit.setBounds(0, 0, contButtonWidth, contButtonHeight);
+        pHeader.add(bExit);
+        // level folder button
+        JButton bOpen = new JButton("level folder");
+        bOpen.setForeground(Color.white);
+        // add action listener to open local level path in file explorer
+        bOpen.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            Desktop.getDesktop().open(levelSelect.getUserData().getLevelPath());
+                        } catch (IOException e1) {
+                            System.out.println("Error: " + e1.getMessage());
+                        }
+                    }
+                }
+        );
+        bOpen.setBackground(cButton);
+        bOpen.setBounds(0, height -  2 * contButtonHeight, contButtonWidth, contButtonHeight);
+        pBody.add(bOpen);
+        // refresh levels button
+        JButton bRefresh = new JButton("refresh");
+        bRefresh.setForeground(Color.white);
+        // add action listener to tell level select to refresh level data
+        bRefresh.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        // reload levels, this also sorts them by name A-Z
+                        levelSelect.loadLevelsCards();
+                        // reset sort dropdown
+                        bSort.setSelectedIndex(0);
+                        // refresh list graphics
+                        refreshList();
+                    }
+                }
+        );
+        bRefresh.setBackground(cButton);
+        bRefresh.setBounds(contButtonWidth + cardWidth - 3 * pageNumberWidth, 0, contButtonWidth, contButtonHeight);
+        pHeader.add(bRefresh);
+        // text labels
+        // header text
+        JLabel lHeader = new JLabel("Level Select");
+        lHeader.setForeground(Color.white);
+        lHeader.setBounds(contButtonWidth, 0, cardWidth - pageNumberWidth, contButtonHeight);
+        pHeader.add(lHeader);
+        // page text
+        pageDisplay = new JLabel("1");
+        pageDisplay.setBackground(cButton);
+        TitledBorder pageBorder = BorderFactory.createTitledBorder("#");
+        pageBorder.setTitleColor(Color.white);
+        pageDisplay.setBorder(pageBorder);
+        pageDisplay.setBounds(contButtonWidth + cardWidth - pageNumberWidth, 0,
+                pageNumberWidth, contButtonHeight);
+        pHeader.add(pageDisplay);
+        // level list
+        listDisplay = new ListDisplay();
+        listDisplay.setBounds(contButtonWidth, 0, cardWidth, height - contButtonHeight);
+        pBody.add(listDisplay);
+        // add panels to level select panel
+        mainPanel.add(pHeader);
+        mainPanel.add(pBody);
+        // refresh card display
+        refreshList();
     }
-
-    // holds color information for graphics
-    private static class Colors {
-        // borders
-        public static final Color border = new Color(61, 61, 61);
-        // color for the header
-        public static final Color headerFill = new Color(145, 145, 145);
-        //color for the cards
-        public static final Color cardFill = new Color(145, 145, 145);
+    // method that re-draws the level card list
+    public void refreshList() {
+        // tell the list display to re-draw list, getting the page cards from level select
+        listDisplay.drawList(levelSelect.getPageCards());
+        // update page number display
+        pageDisplay.setText("" + levelSelect.getPage() + "/" + levelSelect.getNumPages());
+        // redraw level select panel
+        mainPanel.revalidate();
+        mainPanel.repaint();
+    }
+    // class to display and manage the level list UI
+    private class ListDisplay extends JPanel {
+        // constructor, just initializes the list panel
+        public ListDisplay() {
+            // set panel layout
+            this.setLayout(null);
+            // make panel transparent
+            this.setOpaque(false);
+            // set panel border
+            this.setBorder(BorderFactory.createLoweredBevelBorder());
+        }
+        // method to actually draw the card list, takes the list as input (provided from level select)
+        private void drawList(ArrayList<LevelCard> list) {
+            // clear all elements from the list panel
+            this.removeAll();
+            // draw the card list iterating on each card from the list
+            for (final LevelCard card : list) {
+                // create the card button
+                JButton cardButton = new JButton();
+                // add an actionlistener that opens the selected level
+                cardButton.addActionListener(
+                        new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                levelSelect.playLevel(card.getLevel());
+                            }
+                        }
+                );
+                // set button layout and position
+                cardButton.setLayout(null);
+                cardButton.setBackground(cCard);
+                cardButton.setBounds(0, cardHeight * (list.indexOf(card)), cardWidth, cardHeight);
+                // create a card display panel using the level and add it to the button
+                cardButton.add(cardGetDisplay(card));
+                // add card button to the list panel
+                this.add(cardButton);
+            }
+        }
+        // method that returns a card display (containing the title text, author, song duration, etc.) for a given card
+        private JPanel cardGetDisplay(LevelCard card) {
+            // initialize card panel
+            JPanel display = new JPanel();
+            // set panel layout to null and set dimensions
+            display.setLayout(null);
+            display.setBounds(0, 0, 600, 80);
+            // make panel transparent
+            display.setOpaque(false);
+            // initialize card header panel and add it to card panel
+            JPanel header = new JPanel();
+            header.setLayout(null);
+            header.setBounds(0, 0, 480, 80);
+            header.setOpaque(false);
+            display.add(header);
+            // initialize score area panel and add it to card panel
+            JPanel scoreArea = new JPanel();
+            scoreArea.setLayout(null);
+            scoreArea.setBounds(480, 0, 120, 80);
+            scoreArea.setOpaque(false);
+            display.add(scoreArea);
+            // text elements
+            // player highscore
+            JLabel score = new JLabel("" + card.getHighScore());
+            score.setForeground(Color.white);
+            score.setBounds(0, 0, 120, 20);
+            scoreArea.add(score);
+            // player grade
+            JLabel grade = new JLabel("" + card.getGrade());
+            grade.setForeground(Color.white);
+            grade.setBounds(0, 20, 120, 40);
+            scoreArea.add(grade);
+            // level title
+            JLabel title = new JLabel(card.getTitle());
+            title.setForeground(Color.white);
+            title.setBounds(0, 0, 252, 27);
+            header.add(title);
+            // level author
+            JLabel author = new JLabel(card.getCreator());
+            author.setForeground(Color.white);
+            author.setBounds(0, 27, 360, 26);
+            header.add(author);
+            // song duration
+            JLabel duration = new JLabel(card.getDuration());
+            duration.setForeground(Color.white);
+            duration.setBounds(0, 53, 360, 26);
+            header.add(duration);
+            // difficulty display
+            for (int i = 1; i <= 4; i ++) {
+                // create a difficulty 'star' panel (this can be changed to an icon later)
+                JPanel star = new JPanel();
+                // position the panel using index
+                star.setBounds(252 + 27 * (i-1), 0, 27, 27);
+                // set panel color according to level difficulty
+                if (i <= card.getDifficulty()) star.setBackground(cDiffStar);
+                else star.setBackground(cDiffCont);
+                // add panel to the header panel
+                header.add(star);
+            }
+            // return the card panel
+            return display;
+        }
     }
 }
